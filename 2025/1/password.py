d60e59eceef73bd4bcaeafca8ea3e5545de5fd0f
MartinWang12789 <3944078+MartinWang12789@users.noreply.github.com>
2025-12-05 19:55:49 -0700

    feat: days 1 and 2

---
+++ 2025/1/password.py
@@ -0,0 +1,67 @@
+def count_zeros(file_name: str) -> int:
+    dial = 50  # starts at 50
+    zero_count = 0
+
+    file = open(file_name, "r")
+    for line in file:
+        dir = line[0]
+        clicks = int(line[1:])
+        if dir == 'L':
+            dial = (dial - clicks) % 100
+        elif dir == 'R':
+            dial = (dial + clicks) % 100
+        
+        if dial == 0:
+            zero_count += 1
+
+    return zero_count
+
+
+def count_intervening_zeros(file_name: str) -> int:
+    dial = 50  # starts at 50
+    zero_count = 0
+
+    file = open(file_name, "r")
+    for line in file:
+        dir = line[0]
+        clicks = int(line[1:])
+        
+        intervening_count = 0
+        if dir == 'L':
+            if dial != 0:
+                temp_dial = dial - clicks - 100  # guarantee negative and account for crossing 0
+            else:
+                temp_dial = dial - clicks  # don't count 0 twice
+            intervening_count += temp_dial // -100
+            dial = temp_dial % 100
+        elif dir == 'R':
+            temp_dial = dial + clicks
+            intervening_count += temp_dial // 100
+            dial = temp_dial % 100
+        
+        #print(f"debug: {intervening_count}")
+        zero_count += intervening_count
+
+    return zero_count
+
+
+if __name__ == "__main__":
+    file_path = "2025/1/"
+
+    # Pt 1
+    file_name = file_path + "example.txt"
+    result = count_zeros(file_name)
+    print(f"result: {result}")
+
+    file_name = file_path + "input.txt"
+    result = count_zeros(file_name)
+    print(f"result: {result}")
+
+    # Pt 2
+    file_name = file_path + "example.txt"
+    result = count_intervening_zeros(file_name)
+    print(f"result: {result}")
+    
+    file_name = file_path + "input.txt"
+    result = count_intervening_zeros(file_name)
+    print(f"result: {result}")
---
+++ 2025/2/invalid.py
@@ -0,0 +1,74 @@
+def sum_invalids(file_name: str) -> int:
+    invalids_sum = 0
+
+    file = open(file_name, "r")
+    for line in file:
+        rules = line.split(',')
+
+        for rule in rules:
+            low, high = rule.split('-')
+
+            for id in range(int(low), int(high) + 1):
+                length = len(str(id))
+                if length % 2 == 0:
+                    mid = length // 2
+                    id_string = str(id)
+                    front = id_string[:mid]
+                    back = id_string[mid:]
+                    if front == back:
+                        invalids_sum += id
+                    # print(f"debug: {id}: front: {front} back: {back}")
+
+    return invalids_sum
+
+
+def sum_invalids_any_pattern(file_name: str) -> int:
+    invalids_set = set()
+
+    file = open(file_name, "r")
+    for line in file:
+        rules = line.split(',')
+
+        for rule in rules:
+            low, high = rule.split('-')
+
+            for id in range(int(low), int(high) + 1):
+                length = len(str(id))
+
+                if length > 1:
+                    for modulo in range(2, length + 1):
+                        if length % modulo == 0:
+                            sub_length = length // modulo
+                            id_string = str(id)
+                            items = [id_string[i:i + sub_length] for i in range(0, length, sub_length)]
+
+                            if all([item == items[0] for item in items]):
+                                invalids_set.add(id)
+                                # print(f"debug: {id} added to set")
+                            # print(f"debug: {id}: sub_length: {sub_length} items: {items}")
+    
+    invalids_sum = sum(invalids_set)
+
+    return invalids_sum
+
+
+if __name__ == "__main__":
+    file_path = "2025/2/"
+
+    # Pt 1
+    file_name = file_path + "example.txt"
+    result = sum_invalids(file_name)
+    print(f"result: {result}")
+
+    file_name = file_path + "input.txt"
+    result = sum_invalids(file_name)
+    print(f"result: {result}")
+
+    # Pt 2
+    file_name = file_path + "example.txt"
+    result = sum_invalids_any_pattern(file_name)
+    print(f"result: {result}")
+    
+    file_name = file_path + "input.txt"
+    result = sum_invalids_any_pattern(file_name)
+    print(f"result: {result}")
